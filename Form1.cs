99a98abe1beb7fa10a216c68f9ca3aea4033b963
MariaErmolaeva <MariaErmolaeva@users.noreply.github.com>
2019-10-22 13:10:57 +0500

    class Main Form

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App2
+{
+    public partial class Form1 : Form
+    {
+        Presenter presentor;
+
+        List<Characters> characters;
+
+        public Form1()
+        {
+            InitializeComponent();
+            presentor = new Presenter(new Action());
+            characters = new List<Characters>();
+        }
+
+        private void Attack_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void addCharacters_Click(object sender, EventArgs e)
+        {
+            
+
+            
+        }
+    }
+}
