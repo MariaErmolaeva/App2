bd104e98412321e7c01396c255c4f59e8ffcd388
MariaErmolaeva <MariaErmolaeva@users.noreply.github.com>
2019-10-22 13:03:37 +0500

    New version interface IAction

--- IAction.cs
+++ IAction.cs
@@ -2,39 +2,11 @@ namespace App2
 {
     public interface IAction
     {
-        string Battle(int distance); 
-        string Information();
-        string Run(int distance);
+        int Run(int distance, Characters characters);
+        int Attack(Characters characters);
+        int Health(Characters characters);
+
+        void CreateCharacters(string name, string race, string age, List<Characters> characters);
     }
 
-
-    public class Action : IAction
-    {
-        private Mage mage;
-        private Archer archer;
-        private Warrior warrior;
-
-        public Action(Mage mage, Archer archer, Warrior warrior)
-        {
-            this.mage = mage;
-            this.archer = archer;
-            this.warrior = warrior;
-        }
-
-        public string Battle(int distance)
-        {
-
-            return "";
-        }
-
-        public string Information()
-        {
-            return "";
-        }
-
-        public string Run(int distance)
-        {
-            return "";
-        }
-    }
 }
