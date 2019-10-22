6d964796ce71bd460ccbbad31c4e2252d6f8bf6b
MariaErmolaeva <MariaErmolaeva@users.noreply.github.com>
2019-10-22 13:08:09 +0500

    Add health, attack and time

--- Characters.cs
+++ Characters.cs
@@ -13,10 +13,18 @@
         private string name;
         private int age;
         private Race race;
+		
+		private int attack;
+        private int health;
+        private int time;
 
         public string nameGet { get { return name; } }
         public int ageGet { get { return age; } }
         public Race raceGet { get { return race; } }
+		
+		public int attackSetGet { get { return attack; } set { attack = value; } }
+        public int timeSetGet { get { return time; } set { time = value; } }
+        public int healthSetGet { get { return health; } set { health = value; } }
 
         public Characters(string name, int age, Race race)
         {
