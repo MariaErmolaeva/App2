94e90f0cbe870fefe96349a36eb3a4f5fb837965
MariaErmolaeva <MariaErmolaeva@users.noreply.github.com>
2019-10-22 17:26:02 +0500

    Create instance Action, Form and Presentor Run form

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App2
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Form1 form = new Form1();
+            Action action = new Action();
+
+            Presenter presenter = new Presenter(action, form);
+
+            Application.Run(form);
+        }
+    }
+}
