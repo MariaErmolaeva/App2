e576961b44e42d23c5e94d81844f4448eda8ce99
MariaErmolaeva <MariaErmolaeva@users.noreply.github.com>
2019-10-22 17:27:29 +0500

    add EventHandler for button add properties

--- IMainView.cs
+++ IMainView.cs
@@ -1,11 +1,23 @@
-﻿
+﻿using System;
+
 namespace App2
 {
     public interface IMainView
     {
-        string OnSuccessRun(Characters characters);
-        string OnSuccessAttack(Characters characters);
-        string OnSuccessHealth(Characters characters);
-        string OnSuccessAddCharacters(Characters characters);
+        void OnSuccessRun(Characters characters);
+        void OnSuccessAttack(Characters characters);
+        void OnSuccessHealth(Characters characters);
+        void OnSuccessCreateCharacters(Characters characters);
+
+        int index { get; }
+        string nameCharacters { get; }
+        string race { get; }
+        string type { get; }
+
+        //события кнопок
+        event EventHandler AttackClick;
+        event EventHandler RunClick;
+        event EventHandler HealthClick;
+        event EventHandler CreateCharactersClick;
     }
 }
